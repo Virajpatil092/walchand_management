bd2bc6e78bc307a3f5e24b29794ff0e37b9a09c8
viraj <viraj05082003patil@gmail.com>
2023-04-06 23:50:17 +0530

    did some final changes for data view

--- src/components/DirectorHome.js
+++ src/components/DirectorHome.js
@@ -159,48 +159,50 @@ const DirectorHome = (props) => {
         <div className="my-5 d-flex justify-content-center">
             <button type="button" className={`btn btn-primary ${(course === "null")?'disabled':''}`} onClick={handleonclick}>Add Filter</button>
         </div>
-        <div className="">
+        <div className="my-5">
           {data ? (
             <div className='my-5'>
-            <h2>API Data:</h2>
-            <ul class="responsive-table">
-                    <li className="table-header">
-                      <div className="col col-1">Name</div>
-                      <div className="col col-2">Position</div>
-                      <div className="col col-3">Department</div>
-                      <div className="col col-4">Class Name</div>
-                      <div className="col col-5">Activity Name</div>
-                      <div className="col col-6">Mode of Attendance</div>
-                      <div className="col col-7">Description</div>
-                      <div className="col col-8">Start Date</div>
-                      <div className="col col-9">End Date</div>
-                      <div className="col col-10">Location</div>
-                      <div className="col col-11">Category</div>
-                      <div className="col col-12">Participants Count</div>
-                      <div className="col col-13">Outcomes</div>
-                      <div className="col col-14">File</div>
-                    </li>
-              </ul>
             {data.map((item) => (
-              <div className='my-5 api-item border shadow-lg h5' key={item.id}>
-                  <ul className="responsive-table">
-                    <li className="table-row">      
-                      <div className="col col-1" >{item.name}</div>
-                      <div className="col col-2" >{item.position}</div>
-                      <div className="col col-3" >{item.department}</div>
-                      <div className="col col-4" >{item.activity_name}</div>
-                      <div className="col col-5" >{item.mode_of_attendance}</div>
-                      <div className="col col-6" >{item.description}</div>
-                      <div className="col col-7" >{item.department}</div>
-                      <div className="col col-8" >{item.start_date}</div>
-                      <div className="col col-9" >{item.end_date}</div>
-                      <div className="col col-10" >{item.location}</div>
-                      <div className="col col-11" >{item.category}</div>
-                      <div className="col col-12" >{item.participants_count}</div>
-                      <div className="col col-13" >{item.outcomes}</div>
-                      <div className="col col-14" >{item.file}</div>
-                    </li>
-                  </ul>
+              <div id='directorview' className='my-5 api-item border shadow-lg h5 table-responsive container' key={item.id}>
+
+              <table className="table table-bordered">
+              <thead className='thead-dark'>
+                <tr>
+                      <th className="col col-1">Name</th>
+                      <th className="col col-2">Position</th>
+                      <th className="col col-3">Department</th>
+                      <th className="col col-4">Class Name</th>
+                      <th className="col col-5">Activity Name</th>
+                      <th className="col col-6">Mode of Attendance</th>
+                      <th className="col col-7">Description</th>
+                      <th className="col col-8">Start Date</th>
+                      <th className="col col-9">End Date</th>
+                      <th className="col col-10">Location</th>
+                      <th className="col col-11">Category</th>
+                      <th className="col col-12">Participants Count</th>
+                      <th className="col col-13">Outcomes</th>
+                      <th className="col col-14">File</th>
+                </tr>
+              </thead>
+                <tbody>
+                  <tr>
+                      <th className="col col-1" >{item.name ? item.name : 'N/A'}</th>
+                      <th className="col col-2" >{item.position ? item.position : 'N/A'}</th>
+                      <th className="col col-3" >{item.department ? item.department : 'N/A'}</th>
+                      <th className="col col-4" >{item.class_name ? item.class_name : 'N/A'}</th>
+                      <th className="col col-5" >{item.activity_name ? item.activity_name : 'N/A'}</th>
+                      <th className="col col-6" >{item.mode_of_attendance ? item.mode_of_attendance : 'N/A'}</th>
+                      <th className="col col-7" >{item.description ? item.description : 'N/A'}</th>
+                      <th className="col col-8">{item.start_date ? new Date(item.start_date).toLocaleDateString() : "N/A"}</th>
+                      <th className="col col-8">{item.end_date ? new Date(item.end_date).toLocaleDateString() : "N/A"}</th>
+                      <th className="col col-10" >{item.location ? item.location : 'N/A'}</th>
+                      <th className="col col-11" >{item.category ? item.category : 'N/A'}</th>
+                      <th className="col col-12" >{item.participants_count ? item.participants_count : 'N/A'}</th>
+                      <th className="col col-13" >{item.outcomes ? item.outcomes : 'N/A'}</th>
+                      <th className="col col-14" >{item.file ? item.file : 'N/A'}</th>
+                  </tr>
+                  </tbody>
+                </table>
               </div>
             ))}
           </div>
