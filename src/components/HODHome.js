d457307f561e6accaedba4d4dd18271fb80a4146
viraj <viraj05082003patil@gmail.com>
2023-04-12 00:33:54 +0530

    updated the view

--- src/components/HODHome.js
+++ src/components/HODHome.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { Card, Table } from 'react-bootstrap';
 
 
 const HODHome = (props) => {
     const Navigate = useNavigate();
     const [data, setdata] = useState([]);
     const url = 'http://localhost:8000/forms/getbranchwise';
+    const [obj, setobj] = useState();
 
     useEffect(() => {
         const department = localStorage.getItem('branch');
@@ -50,6 +52,62 @@ const HODHome = (props) => {
 
     return (
         <>
+        {obj && 
+        <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+              <Table striped bordered hover responsive>
+              <thead className='thead-dark'>
+                <tr>
+                  <th>Name</th>
+                  <th>Position</th>
+                  <th>Department</th>
+                  <th>Class Name</th>
+                  <th>Activity Name</th>
+                  <th>Mode of Attendance</th>
+                  <th>Description</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                  <th>Location</th>
+                  <th>Category</th>
+                  <th>Participants Count</th>
+                  <th>Outcomes</th>
+                  <th>File</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  <>
+                    <td>{obj.name ? obj.name : 'N/A'}</td>
+                    <td>{obj.position ? obj.position : 'N/A'}</td>
+                    <td>{obj.department ? obj.department : 'N/A'}</td>
+                    <td>{obj.class_name ? obj.class_name : 'N/A'}</td>
+                    <td>{obj.activity_name ? obj.activity_name : 'N/A'}</td>
+                    <td>{obj.mode_of_attendance ? obj.mode_of_attendance : 'N/A'}</td>
+                    <td>{obj.description ? obj.description : 'N/A'}</td>
+                    <td>{obj.start_date ? new Date(obj.start_date).toLocaleDateString() : "N/A"}</td>
+                    <td>{obj.end_date ? new Date(obj.end_date).toLocaleDateString() : "N/A"}</td>
+                    <td>{obj.location ? obj.location : 'N/A'}</td>
+                    <td>{obj.category ? obj.category : 'N/A'}</td>
+                    <td>{obj.participants_count ? obj.participants_count : 'N/A'}</td>
+                    <td>{obj.outcomes ? obj.outcomes : 'N/A'}</td>
+                    <td>{obj.file ? obj.file : 'N/A'}</td>
+                    </>
+                }
+              </tbody>
+            </Table>
+              </div>
+            </div>
+          </div>
+        </div>
+        }
         <div className="d-flex flex-column align-items-center justify-content-center h-100 position-relative">
                 <button onClick={handleOnClick} type="button" className="btn btn-primary">
                     Upload New Event
@@ -57,55 +115,27 @@ const HODHome = (props) => {
         </div>
 
         <div className="my-5">
-          {data ? (
-            <div className='my-5'>
-            {data.map((item) => (
-              <div id='directorview' className='my-5 api-item border shadow-lg h5 table-responsive container' key={item.id}>
-
-              <table className="table table-bordered">
-              <thead className='thead-dark'>
-                <tr>
-                      <th className="col col-1">Name</th>
-                      <th className="col col-2">Position</th>
-                      <th className="col col-3">Department</th>
-                      <th className="col col-4">Class Name</th>
-                      <th className="col col-5">Activity Name</th>
-                      <th className="col col-6">Mode of Attendance</th>
-                      <th className="col col-7">Description</th>
-                      <th className="col col-8">Start Date</th>
-                      <th className="col col-9">End Date</th>
-                      <th className="col col-10">Location</th>
-                      <th className="col col-11">Category</th>
-                      <th className="col col-12">Participants Count</th>
-                      <th className="col col-13">Outcomes</th>
-                      <th className="col col-14">File</th>
-                </tr>
-              </thead>
-                <tbody>
-                  <tr>
-                      <th className="col col-1" >{item.name ? item.name : 'N/A'}</th>
-                      <th className="col col-2" >{item.position ? item.position : 'N/A'}</th>
-                      <th className="col col-3" >{item.department ? item.department : 'N/A'}</th>
-                      <th className="col col-4" >{item.class_name ? item.class_name : 'N/A'}</th>
-                      <th className="col col-5" >{item.activity_name ? item.activity_name : 'N/A'}</th>
-                      <th className="col col-6" >{item.mode_of_attendance ? item.mode_of_attendance : 'N/A'}</th>
-                      <th className="col col-7" >{item.description ? item.description : 'N/A'}</th>
-                      <th className="col col-8">{item.start_date ? new Date(item.start_date).toLocaleDateString() : "N/A"}</th>
-                      <th className="col col-8">{item.end_date ? new Date(item.end_date).toLocaleDateString() : "N/A"}</th>
-                      <th className="col col-10" >{item.location ? item.location : 'N/A'}</th>
-                      <th className="col col-11" >{item.category ? item.category : 'N/A'}</th>
-                      <th className="col col-12" >{item.participants_count ? item.participants_count : 'N/A'}</th>
-                      <th className="col col-13" >{item.outcomes ? item.outcomes : 'N/A'}</th>
-                      <th className="col col-14" >{item.file ? item.file : 'N/A'}</th>
-                  </tr>
-                  </tbody>
-                </table>
+      {data ? (
+        <>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {data.slice(0).reverse().map((item) => (
+              <div key={item.id} className="col">
+                <Card className='shadow p-3 mb-5 bg-white rounded'>
+                  <Card.Header>{item.name ? item.name : 'N/A'}</Card.Header>
+                  <Card.Body>
+                    <Card.Title>{item.activity_name ? item.activity_name : 'N/A'}</Card.Title>
+                    <Card.Text>{item.description ? item.description : 'N/A'}</Card.Text>
+                    <button type="button" className="btn btn-primary" onClick={()=>{setobj(item)}} data-toggle="modal" data-target="#exampleModalCenter">View Details</button>
+                  </Card.Body>
+                </Card>
               </div>
             ))}
           </div>
-
-          ) : (
-          <p>No data available</p>
+          
+          </>
+          ):
+          (
+            <></>
           )}
         </div>
 
